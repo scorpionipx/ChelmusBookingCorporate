974f942ff3aef7c82f48eeafff33b39b9029f5d2
DESKTOP-PE8APUQ\ScorpionIPX <scorpionipx@gmail.com>
2017-09-09 00:20:23 +0300

    Project build up

---
+++ ChelmusBooking/migrate.bat
@@ -0,0 +1,4 @@
+color b
+@echo off
+cls
+cmd /k "cd /d D:\Documents\Python\Django\ChelmusBookingCorporate\virtualenv\Scripts & activate & cd /d    D:\Documents\Python\Django\ChelmusBookingCorporate\ChelmusBooking & python manage.py migrate"
---
+++ ChelmusBooking/run_server_default.bat
@@ -0,0 +1,4 @@
+color a
+@echo off
+cls
+cmd /k "cd /d D:\Documents\Python\Django\ChelmusBookingCorporate\virtualenv\Scripts & activate & cd /d    D:\Documents\Python\Django\ChelmusBookingCorporate\ChelmusBooking & python manage.py runserver"
